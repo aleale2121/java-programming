8de7edcd9bf846fba8c80439c01df4dcbb005a4e
alefew <alefewyimer4@gmail.com>
2024-04-23 00:00:46 +0300

    YourFirstAccount

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,13 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account artosSwissAccount = new Account("Alefew", 100.00);
+
+        artosSwissAccount.deposit(20.0);
+        System.out.println(artosSwissAccount.toString());
+
+    }
+}
