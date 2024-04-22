e515b62e86f4869a39473572719407c0da0806aa
alefew <alefewyimer4@gmail.com>
2024-04-22 20:07:16 +0300

    ComparingNumbers

---
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class ComparingNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int secondNumber = scanner.nextInt();
+
+        if (firstNumber > secondNumber) {
+            System.out.println(firstNumber + " is greater than " + secondNumber + ".");
+        } else if (firstNumber < secondNumber) {
+            System.out.println(firstNumber + " is smaller than " + secondNumber + ".");
+        } else {
+            System.out.println(firstNumber + " is equal to " + secondNumber + ".");
+        }
+
+        scanner.close();
+    }
+}
