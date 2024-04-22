9dab76b35f609e0920d2a79f99faba7691e5f93c
alefew <alefewyimer4@gmail.com>
2024-04-22 19:58:34 +0300

    Dinosaur

---
+++ part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        // Write your program here
+
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+
+    }
+}
