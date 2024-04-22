cbed545571026c9216b4a5726ab4caf846045eaa
alefew <alefewyimer4@gmail.com>
2024-04-22 20:07:18 +0300

    FromParameterToOne

---
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,17 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+        printFromNumberToOne(5);
+//        System.out.println();
+//        printFromNumberToOne(2);
+
+    }
+    
+    public static void printFromNumberToOne(int number) {
+        for (int i = number; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
