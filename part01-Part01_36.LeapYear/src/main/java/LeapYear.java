7726ddb36ac2bc36dcae16586a72bed97d77da3b
alefew <alefewyimer4@gmail.com>
2024-04-22 19:55:46 +0300

    LeapYear

---
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give a year:");
+        int year = scanner.nextInt();
+        boolean leap = false;
+        scanner.close();
+        if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
+            leap = true;
+        } else {
+            leap = false;
+        }
+
+        if (leap) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
+    }
+
+}
