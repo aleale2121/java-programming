3a589ded3bc2d75135b66743c8c5305d9fbecc0d
alefew <alefewyimer4@gmail.com>
2024-04-25 10:55:40 +0300

    TDD Exercises

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,38 @@
 
+import java.util.ArrayList;
+
 public class ExerciseManagement {
 
+    private final ArrayList<Exercise> exercises;
+
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+    }
+
+    public ArrayList<String> exerciseList() {
+        ArrayList<String> list = new ArrayList<>();
+        exercises.forEach((exercise) -> {
+            list.add(exercise.getName());
+        });
+        return list;
+    }
+
+    public void add(String exercisea) {
+        this.exercises.add(new Exercise(exercisea));
+    }
+
+    public void markAsCompleted(String exercise) {
+        exercises.stream().filter((ex) -> (ex.getName().equals(exercise))).forEachOrdered((ex) -> {
+            ex.setCompleted(true);
+        });
+    }
+
+    public boolean isCompleted(String exercise) {
+        for (Exercise ex : exercises) {
+            if (ex.getName().equals(exercise)) {
+                return ex.isCompleted();
+            }
+        }
+        return false;
+    }
 }
